c81b9190b99406fe217b93d283c641e59acc6a86
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2023-05-27 09:48:19 +0300

    All pages are responsive

--- src/components/GaragePage/GarageServices/GarageServices.jsx
+++ src/components/GaragePage/GarageServices/GarageServices.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import AlignedContainer from "../../UI/AlignedContainer/AlignedContainer";
 import Filters from "./Filters/Filters";
 import Services from "./Services/Services";
+import { Tablet } from "../../../globals/responsive";
 
 const Container = styled.section`
   margin-top: 80px;
@@ -12,6 +13,8 @@ const Content = styled.div`
   display: flex;
   flex-direction: column;
   padding: 40px;
+
+  ${Tablet({ padding: 0 })}
 `;
 
 const GarageServices = () => {
