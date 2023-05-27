84f6ba1c9c96a8c51901be7b35597f7ce3794e0b
Yazan-Habash114 <54286064+Yazan-Habash114@users.noreply.github.com>
2023-05-27 09:03:53 +0300

    Questions page is responsive

--- src/pages/QuestionsPage.jsx
+++ src/pages/QuestionsPage.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Background from "../components/QuestionsPage/Background/Background";
 import Wrapper from "../components/QuestionsPage/Wrapper/Wrapper";
+import Header from "../components/UI/Header/Header";
+import Footer from "../components/UI/Footer/Footer";
 
 const QuestionsPage = () => {
   return (
     <>
+      <Header />
       <Background />
       <Wrapper />
+      <Footer />
     </>
   );
 };
